9d6d1a05c1a1293d49abe01542d74b1ef327cc3d
RJ Cunningham <cunningham.rjc@gmail.com>
2017-10-06 09:50:42 -0500

    Fixed some build stuff for the pi.

--- software/easy_build.sh
+++ software/easy_build.sh
@@ -2,5 +2,6 @@
 cd build
 #Uses g++ because it doesn't seem to build right with default.
 #May want to investigate compiler later.
-cmake -B. -H.. -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_FLAGS="CMAKE_CXX_FLAGS -std=c++11"
+cmake -B. -H.. -DCMAKE_CXX_COMPILER=g++
+#-DCMAKE_CXX_FLAGS="CMAKE_CXX_FLAGS -std=c++11"
 make
