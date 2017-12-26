44ccb63bd51ce1b55c3f8f1954e6e50fa9f95f5e
Thomas Yuan <ty19@rice.edu>
2017-12-26 16:02:43 -0600

    In process of adding tabs. Graph updating speed is improved from before.

--- mk1_gui/gui_final.py
+++ mk1_gui/gui_final.py
@@ -1,13 +1,14 @@
 from collections import deque
 from tkinter import PhotoImage
+import matplotlib.pyplot as plt
 from scipy import random
 import tkinter as tk
+import tkinter.ttk as ttk
 from concurrency import async
 from networking import*
 import matplotlib.animation as animation
 from server_info import ServerInfo
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use('TkAgg')
 
@@ -105,34 +106,44 @@ class GUIBackend:
 
 class Plotter:
     def __init__(self, name, xlabel, ylabel, axes, queue_in, data_length):
-
         self.queue_in = queue_in
         self.data_length = data_length
         self.xlist = deque(maxlen=data_length)  # new data to plot on x
         self.xlist.append(0)
         self.ylist = deque(maxlen=data_length)  # new data to plot on y
         self.ylist.append(0)
+
         self.axes = axes
+        # The line we're plotting. We were previously drawing a new line every time
+        self.line = self.axes.plot(self.xlist, self.ylist)[0]
 
         self.name = name
         self.xlabel = xlabel
         self.ylabel = ylabel
 
     def redraw(self):
-        # Clearing is slow but we don't have to manually rescale axes this way
-        self.axes.cla()
-
-        # Resetting the axes titles every time is annoying
+        # This is annoying, but we redraw the axes when we rescale so we have to set these again
         self.axes.set_title(self.name)
         self.axes.set_xlabel(self.xlabel)
         self.axes.set_ylabel(self.ylabel)
-        self.axes.plot(self.xlist, self.ylist)
+
+        # Update the data in our graph instead of drawing a new graph
+        self.line.set_xdata(self.xlist)
+        self.line.set_ydata(self.ylist)
+
+        # Recalculate the x and y ranges. x will always change because it is time, but
+        # only change y if we have new data that is out of bounds.
+        self.axes.relim()
+        if min(self.ylist) < self.axes.get_ylim()[0] or max(self.ylist) > self.axes.get_ylim()[1]:
+            self.axes.autoscale_view(scalex=True, scaley=True)
+        else:
+            self.axes.autoscale_view(scalex=True, scaley=False)
 
     def animate(self, *fargs):
 
         # Randomly generate some data to plot
-        for i in range(10):
-            self.queue_in.put((i, self.xlist[-1] + i))
+        for i in range(1, 11):
+            self.queue_in.put((random.randint(0, 1000), self.xlist[-1] + i))
 
         while self.queue_in.qsize() > 1:
             adc_data, t = self.queue_in.get()
@@ -140,9 +151,10 @@ class Plotter:
             self.xlist.append(t)
             self.ylist.append(adc_data)
 
-            # print ("xlist", self.xlist)
-            # print ("ylist", self.ylist)
-            # print (self.filename, "Avg of last 5 values: ", sum(self.ylist[-5:])/5.0)
+        # print ("name", self.name)
+        # print ("xlist", self.xlist)
+        # print ("ylist", self.ylist)
+        # print (self.filename, "Avg of last 5 values: ", sum(self.ylist[-5:])/5.0)
         self.redraw()
 
 
@@ -154,83 +166,103 @@ class GUIFrontend():
         self.root.resizable(False, False)
         self.root.wm_title("Rice Eclipse Mk-1.1 GUI")
 
-        # This figure contains everything to do with matplotlib on the left hand side
-        self.figure = Figure(figsize=(8, 6), dpi=100)
+        # Create a notebook to manage tabs
+        notebook = ttk.Notebook(self.root)
+        default = ttk.Frame(notebook)
+        loads = ttk.Frame(notebook)
+        pressures = ttk.Frame(notebook)
+        thermos = ttk.Frame(notebook)
+        logging = ttk.Frame(notebook)
 
-        canvas = FigureCanvasTkAgg(self.figure, master=self.root)
+        notebook.add(default, text='Default')
+        notebook.add(loads, text='Load Cells')
+        notebook.add(pressures, text='Pressure Transducers')
+        notebook.add(thermos, text='Thermocouples')
+        notebook.add(logging, text='Logging')
+        notebook.grid(row=1, column=1, sticky='NW')
+
+        # Potential to add styles
+        s = ttk.Style()
+        s.theme_use('default')
+
+        # This figure contains everything to do with matplotlib on the left hand side
+        self.figure, self.axes_list = plt.subplots(nrows=2, ncols=2)
+        self.figure.subplots_adjust(top=.9, bottom=.1, left=.12, right=.95, wspace=.3, hspace=.5)
+        self.figure.set_size_inches(8, 6)
+        self.figure.set_dpi(100)
+
+        # Create a canvas to show this figure under the default tab
+        canvas = FigureCanvasTkAgg(self.figure, master=default)
         canvas.get_tk_widget().grid(row=1, column=1, sticky="NW")
         canvas.show()
 
-        # Create 4 graphs in a 2 by 2 grid
-        top_left = self.figure.add_subplot(221)
-        top_right = self.figure.add_subplot(222)
-        bot_left = self.figure.add_subplot(223)
-        bot_right = self.figure.add_subplot(224)
-        self.figure.subplots_adjust(top=.9, bottom=.1, left=.08, right=.95, wspace=.3, hspace=.5)
+        # Configure the plotters for matplotlib. Change plots and labels here.
+        self.top_left = Plotter("Load Cell 1", "Time (ms)", "Force (N)", self.axes_list[0][0], self.backend.Q_LC1S, 500)
+        self.top_right = Plotter("Load Cell 2", "Time (ms)", "Force (N)", self.axes_list[0][1], self.backend.Q_LC2S, 250)
+        self.bot_left = Plotter("PT Feed", "Time (ms)", "Pressure (PSI)", self.axes_list[1][0], self.backend.Q_FEEDS, 100)
+        self.bot_right = Plotter("Thermocouple 2", "Time (ms)", "Temperature (C)", self.axes_list[1][1], self.backend.Q_TC2S, 10)
 
-        # Configure the plotters for matplotlib
-        self.top_left = Plotter("Load Cell 1", "Time (ms)", "Force (N)", top_left, self.backend.Q_LC1S, 100)
-        self.top_right = Plotter("Load Cell 2", "Time (ms)", "Force (N)", top_right, self.backend.Q_LC2S, 50)
-        self.bot_left = Plotter("PT Feed", "Time (ms)", "Pressure (PSI)", bot_left, self.backend.Q_FEEDS, 25)
-        self.bot_right = Plotter("Thermocouple 2", "Time (ms)", "Temperature (C)", bot_right, self.backend.Q_TC2S, 10)
-
-        self.top_left_anim = animation.FuncAnimation(self.figure, self.top_left.animate, interval=100)
-        self.top_right_anim = animation.FuncAnimation(self.figure, self.top_right.animate, interval=100)
-        self.bot_left_anim = animation.FuncAnimation(self.figure, self.bot_left.animate, interval=100)
-        self.bot_right_anim = animation.FuncAnimation(self.figure, self.bot_right.animate, interval=100)
+        # NOTE: the graphs aren't synchronized right now because new data is generated every time the graph
+        # is updated, and the graphs with less data update faster.
+        # This shouldn't be a problem when we plot our actual data.
+        self.top_left_anim = animation.FuncAnimation(self.figure, self.top_left.animate, interval=10)
+        self.top_right_anim = animation.FuncAnimation(self.figure, self.top_right.animate, interval=10)
+        self.bot_left_anim = animation.FuncAnimation(self.figure, self.bot_left.animate, interval=10)
+        self.bot_right_anim = animation.FuncAnimation(self.figure, self.bot_right.animate, interval=10)
 
         # This frame contains everything to do with buttons and entry boxes on the right hand side
         # TODO there's plenty of space left over for stuff like calibration, ignite time, etc.
         frame = tk.Frame(background="AliceBlue", width=350, height=600)
         frame.grid(row=1, column=2, sticky="NE")
 
-        ip_label = tk.Label(frame, text="IP", background="AliceBlue")
+        ip_label = tk.ttk.Label(frame, text="IP", background="AliceBlue")
         ip_label.place(relx=.15, rely=.05)
 
-        ip_entry = tk.Entry(frame)
+        ip_entry = tk.ttk.Entry(frame)
         ip_entry.config(width=15)
         ip_entry.place(relx=.2, rely=.05)
         ip_entry.insert(tk.END, '192.168.1.137')
 
-        port_label = tk.Label(frame, text="port", background="AliceBlue")
+        port_label = tk.ttk.Label(frame, text="port", background="AliceBlue")
         port_label.place(relx=.6, rely=.05)
 
-        port_entry = tk.Entry(frame)
+        port_entry = tk.ttk.Entry(frame)
         port_entry.config(width=5)
         port_entry.insert(tk.END, '1234')
         port_entry.place(relx=.7, rely=.05)
 
         # Connect and disconnect buttons
-        connect_button = tk.Button(frame, text="Connect",
+        connect_button = tk.ttk.Button(frame, text="Connect",
                                    command=lambda:backend.connect(ip_entry.get(), port_entry.get()))
         connect_button.place(relx=.25, rely=.1)
 
-        disconnect_button = tk.Button(frame, text="Disconnect",
+        disconnect_button = tk.ttk.Button(frame, text="Disconnect",
                                       command=lambda:backend.nw.disconnect())
         disconnect_button.place(relx=.6, rely=.1)
 
         # Buttons for actuating the valve
-        set_valve_button = tk.Button(frame, text="Set Valve",
+        set_valve_button = tk.ttk.Button(frame, text="Set Valve",
                             command=lambda:backend.send(ServerInfo.SET_VALVE))
-        unset_valve_button = tk.Button(frame, text="Unset Valve",
+        unset_valve_button = tk.ttk.Button(frame, text="Unset Valve",
                             command=lambda:backend.send(ServerInfo.UNSET_VALVE))
 
         set_valve_button.place(relx=.25, rely=.45)
         unset_valve_button.place(relx=.5, rely=.45)
 
-        set_ignition_button = tk.Button(frame, text="IGNITE",
+        set_ignition_button = tk.ttk.Button(frame, text="IGNITE",
                                         command=lambda:backend.send(ServerInfo.SET_IGNITION))
         set_ignition_image = PhotoImage(file="ignite.gif")
         set_ignition_button.config(image=set_ignition_image)
         set_ignition_button.image = set_ignition_image
         set_ignition_button.place(relx=.15, rely=.75)
 
-        unset_ignition_button = tk.Button(frame, text="UNIGNITE",
+        unset_ignition_button = tk.ttk.Button(frame, text="UNIGNITE",
                                           command=lambda: backend.send(ServerInfo.UNSET_IGNITION))
         unset_ignition_image = PhotoImage(file="unignite.gif")
         unset_ignition_button.config(image=unset_ignition_image)
         unset_ignition_button.image = unset_ignition_image
         unset_ignition_button.place(relx=.55, rely=.75)
+
 
 frontend = GUIFrontend(GUIBackend(Queue(), Queue(), Queue(), Queue(), Queue(),
                                   Queue(), Queue(), Queue(), Queue(), Queue()))
--- mk1_gui/sampleCode.py
+++ mk1_gui/sampleCode.py
@@ -1,36 +1,18 @@
-import matplotlib
-matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
-from numpy import arange, sin, pi
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from matplotlib.figure import Figure
-import tkinter as Tk
+fig, ax = plt.subplots()
+line, = ax.plot(np.random.randn(100))
 
-root = Tk.Tk()
-root.wm_title("Rice Eclipse Mk-1.1 GUI")
-
-figure = Figure(figsize=(8, 6), dpi=100)
-
-# Create 4 graphs in a 2 by 2 grid
-top_left = figure.add_subplot(221)
-top_left.set_title("abcd")
-top_right = figure.add_subplot(222)
-bot_left = figure.add_subplot(223)
-bot_right = figure.add_subplot(224)
-figure.subplots_adjust(top = .9, bottom=.1, left=.08, right = .95, wspace=.2, hspace=.5)
-
-# generate some dummy data
-t = arange(0.0, 3.0, 0.01)
-s = sin(2*pi*t)
-
-top_left.plot(t, s)
-
-# a tk.DrawingArea
-canvas = FigureCanvasTkAgg(figure, master=root)
-canvas.get_tk_widget().grid(row=1, column=1, sticky="NW")
-canvas.show()
-
-background_frame = Tk.Frame(background = "bisque", width=350, height=600)
-background_frame.grid(row=1, column=2, sticky="NE")
-
-Tk.mainloop()
+fig.canvas.draw()
+tstart = time.time()
+num_plots = 0
+while time.time()-tstart < 1:
+    line.set_ydata(np.random.randn(100))
+    ax.draw_artist(ax.patch)
+    ax.draw_artist(line)
+    # fig.canvas.update()
+    fig.canvas.flush_events()
+    num_plots += 1
+print(num_plots)
